4b7a48dca18a6c9c873520b45f9f591a05a1c27f
gbubemi22 <slimnfine22@gmail.com>
2022-12-31 13:07:07 +0100

    routers

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,7 +5,7 @@ import (
      "net/http"
 	"github.com/gorilla/mux"
 	_"github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/gbubemi22/go-bookstore/pkg/routes"
+	"github.com/gbubemi22/go-bookstore/pkg/routers"
 	
 )
 
